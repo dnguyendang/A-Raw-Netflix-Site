8a6fcd26d3b5b5ad23fd77c58f4493fdb74e4a74
Dangg Dungg <dungduongthanhson@gmail.com>
2024-10-14 21:53:15 +0700

    debug=true

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['https://netflix-site.onrender.com','127.0.0.1']
